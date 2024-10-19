9c91df6f65b398be0e62962c1fb11b2159b07478
kuntakinte-n <kuntakintengcobo@gmail.com>
2024-10-20 01:02:32 +0200

    Upload Drizzle DB Setup

---
+++ src/communication/providers/SMSProvider.ts
@@ -0,0 +1,27 @@
+// import dotenv
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { accountSid, authToken, from } = {
+    accountSid: String(process.env.TWILIO_ACCOUNT_SID),
+    authToken: String(process.env.TWILIO_AUTH_TOKEN),
+    from: String(process.env.TWILIO_PHONE_NUMBER),
+};
+
+// console.log("authToken", authToken)
+// console.log("accountSid", accountSid)
+
+const client = require('twilio')(accountSid, authToken);
+const HOST_NUMBER = String(process.env.TWILIO_PHONE_NUMBER);
+
+export const sendSms = (sender: string, message: string) => client.messages
+  .create({
+    body: message,
+    to: sender, // Text your number
+    from: from, // From a valid Twilio number
+  })
+  .then((message: any) => console.log(message.sid));
+
+// export const sendSms = (sender: string, message: string) => "HB"
+//   .then((message: any) => console.log(message.sid));
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import homeRoutes from './homeRoutes';
 import testRoutes from './testRoutes';
+import communicationRoutes from './communicationRoutes';
 
 const router = express.Router();
 
 router.use('/', homeRoutes);
 router.use('/test', testRoutes);
+router.use('/chatbot', communicationRoutes);
 
 export default router;
